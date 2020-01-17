cb4d0181509bcfe211bb800e96c33ee20f664185
EvgeniiGru <55989098+EvgeniiGru@users.noreply.github.com>
2020-01-17 17:04:44 +0300

    рефакторинг освобождение UI от связи с DAL
    рефакторинг освобождение UI от связи с DAL с помощью thunk creater  в bll уровне

--- client/src/components/Frends/FrendsConteiner.jsx
+++ client/src/components/Frends/FrendsConteiner.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Frends from './Frends'
 import { withRouter } from 'react-router-dom'
-import { changePage, getFrends, ChangeCheckBoxValue } from './../../redux/FrendReduser'
+import { changePage, getFrends,ChangeCheckBoxValue, addFrends, delFrends, unfollowFrends, wayBeFrendDelet } from './../../redux/FrendReduser'
 import Preloader from '../Logo/Preload'
-import { frendsQurePost } from '../../API/apiPost'
-import { frendsQureDelete, unfollow, mayBeFrendDel } from '../../API/apiDel'
 import { withAuthentication } from '../Hoc/withAUHoc'
 import { compose } from 'redux'
 
@@ -20,50 +18,16 @@ class FrendClass extends React.Component {
         this.props.getFrends(this.props.count, pageNumber)
     }
 
-    addFrends = (frendID) => {
-        let Th = this;
-        frendsQurePost(frendID).then(res => {
-            Th.onChangePage(1);
-        });
-    }
-
-    delFrends = (frendID) => {
-       
-        let Th = this;
-        frendsQureDelete(frendID).then(res => {
-            Th.onChangePage(1);
-        });
-    }
-
-    unfollowFrends = (frendID) => {
-        let Th = this;
-        unfollow(frendID).then(res => {
-            Th.onChangePage(1);
-        });
-    }
-
-    wayBeFrendDelet = (frendID) => {
-        let Th = this;
-        mayBeFrendDel(frendID).then(res => {
-            Th.onChangePage(1);
-        })
-    }
-
     render() {
         return (
             <>
                 {this.props.preload ? <Preloader /> : null}
                 <Frends {...this.props}
                     onChangePage={this.onChangePage}
-                    addFrends={this.addFrends}
-                    delFrends={this.delFrends}
-                    unfollowFrends={this.unfollowFrends}
-                    wayBeFrendDelet={this.wayBeFrendDelet} 
                     takeFunction={this.takeFunction}/>
             </>)
     }
 }
-
 
 let mapState = (state) => {
     return {
@@ -73,10 +37,14 @@ let mapState = (state) => {
 
 export default compose(
     connect(mapState, {
+        wayBeFrendDelet,
+        unfollowFrends,
+        addFrends,
+        delFrends,
         getFrends,
         changePage,
         ChangeCheckBoxValue
     }) ,
     withRouter,
     withAuthentication
-  )(FrendClass);;
+  )(FrendClass);
